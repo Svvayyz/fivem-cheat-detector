42a59a884c1c49a778e5d858262818127b2d28f9
Svvayyz <76277320+Svvayyz@users.noreply.github.com>
2024-01-18 03:33:26 +0100

    update

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection.Emit;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static System.Net.WebRequestMethods;
+using System.Runtime.InteropServices;
+
+namespace fivemhackdetector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Scanner | Svvayyz#7153";
+
+            string trustedModules = new WebClient().DownloadString("https://adorablehvh.pl/trusted.txt");
+            bool fivemDetected = false;
+
+            Utilities.Log(Utilities.LogType.SUCCESS, "begun scanning!");
+
+            foreach (Process process in Process.GetProcesses())
+            {
+                if (process.ProcessName.Contains("GTAProcess"))
+                {
+                    fivemDetected = true;
+
+                    Utilities.Log(Utilities.LogType.SUCCESS, $"fount fivem process with id of {process.Id}");
+
+                    foreach (ProcessModule module in process.Modules)
+                    {
+                        if (!trustedModules.Contains(module.ModuleName) && !module.ModuleName.EndsWith(".exe"))
+                            Utilities.Log(Utilities.LogType.WARNING, $"fount a suspicious (unknown) module {module.ModuleName} | MEM: 0x{module.BaseAddress}-0x{module.BaseAddress + module.ModuleMemorySize} (size: {module.ModuleMemorySize})");
+                    }
+                }
+            }
+
+            if (!fivemDetected) 
+                Utilities.Log(Utilities.LogType.ERROR, "fivem not found");
+
+            Utilities.Log(Utilities.LogType.SUCCESS, "finished scanning!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Utilities.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fivemhackdetector
+{
+    internal class Utilities
+    {
+        public enum LogType
+        {
+            SUCCESS,
+            WARNING,
+            ERROR
+        }
+
+        private static Dictionary<LogType, ConsoleColor> colors = new Dictionary<LogType, ConsoleColor>()
+        {
+            { LogType.SUCCESS, ConsoleColor.Green },
+            { LogType.WARNING, ConsoleColor.Yellow },
+            { LogType.ERROR, ConsoleColor.Red }
+        };
+
+        private static Dictionary<LogType, string> prefixes = new Dictionary<LogType, string>()
+        {
+            { LogType.SUCCESS, "success" },
+            { LogType.WARNING, "warning" },
+            { LogType.ERROR, "error" }
+        };
+
+        public static void Log(LogType type, string message)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("[");
+
+            Console.ForegroundColor = colors[type];
+            Console.Write(prefixes[type]);
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("] " + message + "\n");
+        }
+    }
+}
